f6c28d321b94556f0c7da0757a0b02895ddaafcd
Kamila <kamila.aibedullova@gmail.com>
2020-02-05 18:55:30 -0500

    one more commit

---
+++ src/test/java/com/qa/test/Search.java
@@ -0,0 +1,12 @@
+package com.qa.test;
+
+import org.testng.annotations.Test;
+
+public class Search {
+
+    @Test
+    public void searchTest(){
+        System.out.println("test Search page hope");
+    }
+    //lkenf;kvm;emf;lvm;l
+}
