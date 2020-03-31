5eaaea3108c51ac138cd377703d3f347cba7e589
Kamila <kamila.aibedullova@gmail.com>
2020-03-30 23:17:50 -0400

    test

---
+++ SkylinePlaza/3701 S George Mason Dr UNIT 301N, Falls Church, VA 22041 _ Zillow_files/verify.js
@@ -0,0 +1,53 @@
+setTimeout(function(){
+    'use strict';
+    try{
+        var stringifyFunc = null;
+		if(window.JSON){
+			stringifyFunc = window.JSON.stringify;
+		} else {
+			if(window.parent && window.parent.JSON){
+				stringifyFunc = window.parent.JSON.stringify;
+			}
+		}
+		if(!stringifyFunc){
+			return;
+		}
+        var msg = {
+            action: 'notifyBrandShieldAdEntityInformation',
+            bsAdEntityInformation: {
+                brandShieldId:'af9efbf3a23245a283da69897b669dd1',
+                comparisonItems:[{name : 'cmp', value : 17907325},{name : 'plmt', value : 17909260}]
+            }
+        };
+        var msgString = stringifyFunc(msg);
+        var bst2tWin = null;
+
+        var findAndSendMessage = function() {
+            if (!bst2tWin) {
+                var frame = document.getElementById('bst2t_958171788769');
+                if (frame) {
+                    bst2tWin = frame.contentWindow;
+                }
+            }
+
+            if (bst2tWin) {
+                bst2tWin.postMessage(msgString, '*');
+            }
+        };
+
+        findAndSendMessage();
+        setTimeout(findAndSendMessage, 50);
+        setTimeout(findAndSendMessage, 500);
+    } catch(err){}
+}, 10);var dvObj = $dvbs;function np764531(g,i){function d(){function d(){function f(b,a){b=(i?"dvp_":"")+b;e[b]=a}var e={},a=function(b){for(var a=[],c=0;c<b.length;c+=2)a.push(String.fromCharCode(parseInt(b.charAt(c)+b.charAt(c+1),32)));return a.join("")},h=window[a("3e313m3937313k3f3i")];h&&(a=h[a("3g3c313k363f3i3d")],f("pltfrm",a));(function(){var a=e;e={};if (a['pltfrm'])dvObj.registerEventCall(g,a,2E3,true)})()}try{d()}catch(f){}}try{dvObj.pubSub.subscribe(dvObj==window.$dv?"ImpressionServed":"BeforeDecisionRender",g,"np764531",d)}catch(f){}}
+;np764531("af9efbf3a23245a283da69897b669dd1",false);
+
+
+try{__tagObject_callback_958171788769({ImpressionID:"af9efbf3a23245a283da69897b669dd1", ServerPublicDns:"tps618.doubleverify.com"});}catch(e){}
+try{$dvbs.pubSub.publish('BeforeDecisionRender', "af9efbf3a23245a283da69897b669dd1");}catch(e){}
+try{__verify_callback_958171788769({
+ResultID:2,
+Passback:"",
+AdWidth:300,
+AdHeight:250});}catch(e){}
+try{$dvbs.pubSub.publish('AfterDecisionRender', "af9efbf3a23245a283da69897b669dd1");}catch(e){}
